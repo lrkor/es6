0059f44d2c5da8519def88b070f82648c88e4647
Lrkor <1055409319@qq.com>
2019-01-13 22:27:41 +0800

    类的继承 super的使用

--- object/class.js
+++ object/class.js
@@ -1,13 +1,49 @@
-class Person {
-    constructor(name,age){
+class Eat {
+    static eat(type) {
+        console.log(type+'都要吃东西');
+    }
+}
+
+class Person extends Eat {
+    constructor(name, age) {
+        super();
         this.name = name;
         this.age = age;
     }
 
-    sayName(){
+    sayName() {
         console.log(this.name);
     }
+
+//    父类的静态方法会被子类继承
+//     static eat(){
+//         console.log('人都要吃饭');
+//     }
 }
 
-var person1 = new Person('张三','15');
-person1.sayName();
+class Student extends Person {
+    constructor(name, age, id) {
+        super(name, age); // 调用父类的constructor(x, y)
+        this.id = id;
+    }
+
+    //    说id
+    sayId() {
+        console.log(this.id);
+    }
+
+    //    说名字
+    sayName() {
+        return super.sayName();
+    }
+
+}
+
+let person1 = new Person('张三', '15');
+let student = new Student('学生', '15', '1401010076');
+// student.sayId();
+// student.sayName();
+// Student.eat();
+// person1.sayName();
+Person.eat('人');
+Student.eat('学生');
